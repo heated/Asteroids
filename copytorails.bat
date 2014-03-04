04963d9cf529c829b0494444a8d734d31c49b05d
Seth Swenson <swensonsd@gmail.com>
2014-03-04 11:12:00 -0800

    Format high scores

---
+++ copytorails.bat
@@ -0,0 +1,18 @@
+copy /Y C:\asteroids\javascripts\asteroid.js      C:\vardarac_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\asteroids_ui.js  C:\vardarac_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\bullet.js        C:\vardarac_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\game.js          C:\vardarac_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\inherits.js      C:\vardarac_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\moving_object.js C:\vardarac_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\particle.js      C:\vardarac_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\preload.js       C:\vardarac_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\ship.js          C:\vardarac_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\saucer.js        C:\vardarac_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\starfield.js     C:\vardarac_portfolio\app\assets\javascripts\asteroids\
+
+copy /Y C:\asteroids\javascripts\cobbleview.js            C:\vardarac_portfolio\app\assets\javascripts\defaults\
+copy /Y C:\asteroids\javascripts\soundjs-0.5.2.min.js     C:\vardarac_portfolio\app\assets\javascripts\defaults\
+copy /Y C:\asteroids\javascripts\keymaster.js             C:\vardarac_portfolio\app\assets\javascripts\defaults\
+
+copy /Y C:\asteroids\stylesheets\asteroids.css    C:\vardarac_portfolio\app\assets\stylesheets\asteroids\
+copy /Y C:\asteroids\stylesheets\starfield.css    C:\vardarac_portfolio\app\assets\stylesheets\asteroids\
