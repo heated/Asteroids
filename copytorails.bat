e5c88be580a996cfac6143cb686171dd98482ba4
Seth Swenson <swensonsd@gmail.com>
2014-03-04 11:12:00 -0800

    Format high scores

---
+++ copytorails.bat
@@ -0,0 +1,18 @@
+copy /Y C:\asteroids\javascripts\asteroid.js      C:\polaris_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\asteroids_ui.js  C:\polaris_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\bullet.js        C:\polaris_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\game.js          C:\polaris_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\inherits.js      C:\polaris_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\moving_object.js C:\polaris_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\particle.js      C:\polaris_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\preload.js       C:\polaris_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\ship.js          C:\polaris_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\saucer.js        C:\polaris_portfolio\app\assets\javascripts\asteroids\
+copy /Y C:\asteroids\javascripts\starfield.js     C:\polaris_portfolio\app\assets\javascripts\asteroids\
+
+copy /Y C:\asteroids\javascripts\cobbleview.js            C:\polaris_portfolio\app\assets\javascripts\defaults\
+copy /Y C:\asteroids\javascripts\soundjs-0.5.2.min.js     C:\polaris_portfolio\app\assets\javascripts\defaults\
+copy /Y C:\asteroids\javascripts\keymaster.js             C:\polaris_portfolio\app\assets\javascripts\defaults\
+
+copy /Y C:\asteroids\stylesheets\asteroids.css    C:\polaris_portfolio\app\assets\stylesheets\asteroids\
+copy /Y C:\asteroids\stylesheets\starfield.css    C:\polaris_portfolio\app\assets\stylesheets\asteroids\
---
+++ readme.md
@@ -0,0 +1,24 @@
+Asteroids
+
+This is a retro-style remake of the 1979 Atari arcade classic, Asteroids.
+It includes a healthy chunk of awesome technical features, including:
+
+- A physics engine and aiming subroutines!
+- Object-oriented game entities and prototypal inheritance!
+- A Rails-powered high scores back-end!
+- A JavaScript custom-written view-based user interface!
+- HTML Canvas vector graphics and animation!
+- Use of a sweet custom font through CSS!
+- A JS-powered image and sound preloader!
+
+And if you're not really into those, I guess there's some gameplay and stuff in there, too...
+
+- Randomly spawning saucers!
+- Collapsing asteroids with semi-conserved momentum! (sorry Physics majors)
+- Particle effects!
+- Explosions! Thrusters! Lasers!
+
+Feel free to clone or hop onto the Heroku host. High score cheaters welcome and likely to be cleared once a month ;)
+
+Cheers,
+Polaris
